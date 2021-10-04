7a602187c54fb383fd64feb00bc679671ea6ff42
zekiunyildiz <zekiunyildiz@gmail.com>
2021-10-05 01:18:03 +0300

    middleware

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name1',
-    description: 'Photo description',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
 });
 
 const port = 3000;
