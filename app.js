35cba902ffa742d14549d95938d4d24dbadfa305
zekiunyildiz <zekiunyildiz@gmail.com>
2021-10-06 01:54:36 +0300

    Template Engine

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 
+// !Template
+app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+
+
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+
+  res.render('add');
+});
+
 
 const port = 3000;
 app.listen(port, () => {
