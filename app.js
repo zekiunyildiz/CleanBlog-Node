6f696edc91f4ea6c4794e5780996f7f09dc9024b
zekiunyildiz <zekiunyildiz@gmail.com>
2021-10-08 01:13:22 +0300

    ModelAndDynamicContent

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//!Connect DB
+mongoose.connect('mongodb://localhost/clean-blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology:true,
+});
+
 
 // !Template Engine
 app.set('view engine', 'ejs');
@@ -13,26 +22,29 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-
-app.get('/', (req, res) => {
+//!INDEX
+app.get('/', async (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-  res.render('index');
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos:photos
+  });
 });
 
+//!ABOUT
 app.get('/about', (req, res) => {
-
-
   res.render('about');
 });
 
+//!ADD PHOTO
 app.get('/add', (req, res) => {
-
   res.render('add');
 });
 
-app.post('/photos', (req, res) => {
 
-  console.log(req.body);
+//! POST
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
   res.redirect('/')
 });
 
