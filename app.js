6f9ca5aa05bc932c67f9f99a390505594ee276a4
zekiunyildiz <zekiunyildiz@gmail.com>
2021-10-13 02:33:34 +0300

    post guncelleme

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
-const fs = require('fs')
+const fs = require('fs');
+const methodOverride = require('method-override');
 const Photo = require('./models/Photo');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
@@ -14,16 +15,15 @@ mongoose.connect('mongodb://localhost/clean-blog', {
   useUnifiedTopology: true,
 });
 
-
 // !Template Engine
 app.set('view engine', 'ejs');
-
 
 //! Middlewares
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 //!INDEX
 app.get('/', async (req, res) => {
@@ -41,7 +41,6 @@ app.get('/photos/:id', async (req, res) => {
   });
 });
 
-
 //!ABOUT
 app.get('/about', (req, res) => {
   res.render('about');
@@ -52,19 +51,17 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-
 //! POST
 app.post('/photos', async (req, res) => {
   //console.log(req.files.image);
   //await Photo.create(req.body)
   //res.redirect('/')
 
-  const uploadDir = 'public/uploads'
+  const uploadDir = 'public/uploads';
 
   if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir)
+    fs.mkdirSync(uploadDir);
   }
-
 
   let uploadImage = req.files.image;
   let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
@@ -76,6 +73,22 @@ app.post('/photos', async (req, res) => {
     });
     res.redirect('/');
   });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`)
 });
 
 const port = 3000;
