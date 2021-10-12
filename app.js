123f4efba280104481ec96cd78457bf7ce8f1592
zekiunyildiz <zekiunyildiz@gmail.com>
2021-10-13 00:41:29 +0300

    Görsel Yüklemek

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const fs = require('fs')
 const Photo = require('./models/Photo');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
 //!Connect DB
 mongoose.connect('mongodb://localhost/clean-blog', {
   useNewUrlParser: true,
-  useUnifiedTopology:true,
+  useUnifiedTopology: true,
 });
 
 
@@ -19,23 +21,23 @@ app.set('view engine', 'ejs');
 
 //! Middlewares
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
 
 //!INDEX
 app.get('/', async (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-  const photos = await Photo.find({})
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
-    photos:photos
+    photos: photos,
   });
 });
 
-
-app.get('/photos/:id', async(req, res) => {
-  const post = await Photo.findById(req.params.id)
+app.get('/photos/:id', async (req, res) => {
+  const post = await Photo.findById(req.params.id);
   res.render('post', {
-    post
+    post,
   });
 });
 
@@ -53,10 +55,28 @@ app.get('/add', (req, res) => {
 
 //! POST
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)
-  res.redirect('/')
-});
+  //console.log(req.files.image);
+  //await Photo.create(req.body)
+  //res.redirect('/')
 
+  const uploadDir = 'public/uploads'
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir)
+  }
+
+
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
+});
 
 const port = 3000;
 app.listen(port, () => {
