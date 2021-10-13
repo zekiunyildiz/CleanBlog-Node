e22fa9126810fc8d07caa5e950bc3462bf9ed344
zekiunyildiz <zekiunyildiz@gmail.com>
2021-10-14 02:59:41 +0300

    photo delete

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method', {
+  methods:['POST', 'GET']
+}));
 
 //!INDEX
 app.get('/', async (req, res) => {
@@ -90,6 +92,15 @@ app.put('/photos/:id', async (req, res) => {
 
   res.redirect(`/photos/${req.params.id}`)
 });
+
+
+app.delete('/photos/:id', async (req,res) =>{
+  const photo = await Photo.findOne({_id: req.params.id})
+  let deletedImage = __dirname + '/public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id)
+  res.redirect('/')
+})
 
 const port = 3000;
 app.listen(port, () => {
