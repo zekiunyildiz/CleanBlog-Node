9442422da5643452d44dbeb0b0656a52279b7ec6
zekiunyildiz <zekiunyildiz@gmail.com>
2021-10-15 02:20:14 +0300

    pagination

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,12 +1,27 @@
-const Photo = require('../models/Photo')
+const Photo = require('../models/Photo');
 const fs = require('fs');
 
 exports.getAllPhotos = async (req, res) => {
-  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-  const photos = await Photo.find({}).sort('-dateCreated');
+  const page = req.query.page || 1;
+  const photosPerPage = 2;
+
+  const totalPhotos = await Photo.find().countDocuments();
+  const photos = await Photo.find({})
+    .sort('-dateCreated')
+    .skip((page - 1) * photosPerPage)
+    .limit(photosPerPage);
+
   res.render('index', {
     photos: photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photosPerPage),
   });
+
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  /* const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos: photos,
+  }); */
 };
 
 exports.getPhoto = async (req, res) => {
